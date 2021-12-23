1d458c493cf774d4aa2ab75a8a628f774967a73a
unknown <marcin.lapinski05@gmail.com>
2021-12-23 19:56:45 +0100

    add validation to inputs in contact section

--- src/components/pages/Home/Message.js
+++ src/components/pages/Home/Message.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import validator from 'validator';
 
 const msgPlaceholder = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"
 + "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam," + 
@@ -12,12 +13,41 @@ const Message = () => {
         setMessage(e.target.value);
     }
 
+    const validateMessage = () => {
+
+        if (validator.isLength(message, 120, undefined)) {
+            return (
+                <div className="validation-good">X</div>
+            )
+        } else if (message === "") {
+            return (
+                <div className="validation-good">X</div>
+            )
+        } else {
+            return (
+                <div className="validation-bad">Wiadomość musi składać się z min. 120 znaków!</div>
+            )
+        }
+    }
+
+    const checkStyleMessage = () => {
+        let style;
+        if (message !== "" && !validator.isLength(message, 120, undefined)) {
+            return style = {borderBottom: "2px solid red"}
+        } else if (message === "" || validator.isLength(message, 120, undefined)) {
+            return style = {borderBottom: "2px solid #737373"}
+        } else {
+            return style = {borderBottom: "2px solid #737373"}
+        }
+    }
+
     return (
         <div className="message-textarea">
             <label className="label-message">
                 Wpisz swoją wiadomość
-                <textarea value={message} onChange={messageChange} />
+                <textarea value={message} onChange={messageChange} style={checkStyleMessage()}/>
             </label>
+            {validateMessage()}
         </div>
     )
 }
--- src/components/pages/Home/NameEmail.js
+++ src/components/pages/Home/NameEmail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import validator from 'validator';
 
 const NameEmail = () => {
     const [name, setName] = useState("");
@@ -9,20 +10,82 @@ const NameEmail = () => {
         setName(e.target.value);
     }
 
+    const validateName = () => {
+
+        if (!validator.contains(name, " ", { ignoreCase: false, minOccurrences: 1 })) {
+            return (
+                <div className="validation-good">X</div>
+            )
+        } else if (name === "") {
+            return (
+                <div className="validation-good">X</div>
+            )
+        } else {
+            return (
+                <div className="validation-bad">Imię musi być jednym wyrazem!</div>
+            )
+        }
+    }
+
+    const checkStyleName = () => {
+        let style;
+        if (name !== "" && validator.contains(name, " ", { ignoreCase: false, minOccurrences: 1 })) {
+            return style = {borderBottom: "2px solid red"}
+        } else if (name === "" && !validator.contains(name, " ", { ignoreCase: false, minOccurrences: 1 })) {
+            return style = {borderBottom: "2px solid #737373"}
+        } else {
+            return style = {borderBottom: "2px solid #737373"}
+        }
+    }
+
     const emailChange = (e) => {
         setEmail(e.target.value);
     }
 
+    const validateEmail = () => {
+
+        if (validator.isEmail(email)) {
+            return (
+                <div className="validation-good">X</div>
+            )
+        } else if (email === "") {
+            return (
+                <div className="validation-good">X</div>
+            )
+        } else {
+            return (
+                <div className="validation-bad">Podany email jest nieprawidłowy!</div>
+            )
+        }
+    }
+
+    const checkStyleEmail = () => {
+        let style;
+        if (email !== "" && !validator.isEmail(email)) {
+            return style = {borderBottom: "2px solid red"}
+        } else if (email === "" || validator.isEmail(email)) {
+            return style = {borderBottom: "2px solid #737373"}
+        } else {
+            return style = {borderBottom: "2px solid #737373"}
+        }
+    }
+
     return (
         <div className="name-email">
+            <div>
             <label className="label-name">
                 Wpisz swoje imię
-                <input type="text" placeholder="Krzysztof" value={name} onChange={nameChange} />
+                <input type="text" placeholder="Krzysztof" value={name} onChange={nameChange} style={checkStyleName()} />
             </label>
+            {validateName()}
+            </div>
+            <div>
             <label className="label-email">
                 Wpisz swój email
-                <input type="email" placeholder="abc@xyz.pl" value={email} onChange={emailChange} />
+                <input type="email" placeholder="abc@xyz.pl" value={email} onChange={emailChange} style={checkStyleEmail()} />
             </label>
+            {validateEmail()}
+            </div>
         </div>
     )
 }
